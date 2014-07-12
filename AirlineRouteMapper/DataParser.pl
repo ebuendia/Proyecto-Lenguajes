08209a357c4098c1e8ffb34e8465c8b23aa2b155
Dimitri Laaz PC <sign_assasin@hotmail.com>
2014-07-11 22:40:38 -0500

    termine el parseo de los datos , el archivo final se llama datosparseados.txt y el formato  se encuentra en formato de parseo.txt

--- AirlineRouteMapper/DataParser.pl
+++ AirlineRouteMapper/DataParser.pl
@@ -2,8 +2,8 @@
 
 use 5.010;
 use strict;
-# use warnings;
-
+use warnings;
+use Math::Trig;
 sub readInput
 {
 	
@@ -36,7 +36,7 @@ sub createRoutes
 {
 	my @file = @_;
 	my @result;
-	open (MYFILE, '>output.txt');
+	open (MYFILE, '>datosparseados.txt');
 	my @tmp1,my @tmp2;
 	
 	my %airinfo = loadAirportInfo("airlocations.tsv");
@@ -47,16 +47,48 @@ sub createRoutes
 		# print $line;
 		# my @parameters= split(/\W+/,$line);
 		my @rparameters= split(":",$line);
-		# @tmp1= split(":",$airinfo{$rparameters[1]});
-		# @tmp2= split(":",$airinfo{$rparameters[2]});
+		if(defined $airinfo{$rparameters[1]} and defined $airinfo{$rparameters[2]})
+		{
+			@tmp1= split(":",$airinfo{$rparameters[1]});
+			@tmp2= split(":",$airinfo{$rparameters[2]});
+			my $distance = calcDistance($tmp1[1],$tmp1[2],$tmp2[1],$tmp2[2]);
+			my $tiempo = calcTime($distance);
+			print MYFILE $tmp1[0] . ":";
+			print MYFILE $tmp1[3] . ":";
+			print MYFILE $tmp1[4] . ":";
+			print MYFILE $tmp1[5] . ":";
+			print MYFILE $tmp2[0] . ":";
+			print MYFILE $tmp2[3] . ":";
+			print MYFILE $tmp2[4] . ":";
+			print MYFILE $tmp2[5] . ":";
+			print MYFILE $distance . ":";
+			print MYFILE $tiempo . ":";
+			print MYFILE "\n";
+		}
+		
 		# print $tmp1[0]. " " . $tmp2[0] . "\n";
 		# print $rparameters[1]. " " . $rparameters[2] . "\n";
 		# print $airinfo{$rparameters[1]} .  " " . $airinfo{$rparameters[2]} . "\n";
 	}
-	if(defined $airinfo{"GYE"})
-	{print "si esta vacio";}
-}
 
+}
+sub calcDistance
+{
+	my ($latitud1,$longitud1,$latitud2,$longitud2) = @_;
+	my $radiotierra=6371;
+	my $dlat= deg2rad($latitud2-$latitud1);
+	my $dlon= deg2rad($longitud2-$longitud1);
+	my $a = (sin($dlat/2) * sin($dlat/2)) + (cos(deg2rad($latitud1)) * cos(deg2rad($latitud2)) * sin($dlon/2) * sin($dlon/2));
+	my $c = 2 * atan2(sqrt($a),sqrt(1-$a));
+	my $d = $radiotierra * $c;
+	# return sin(deg2rad(90));
+	return $d;
+	
+}
+sub calcTime
+{
+	return $_[0]/890;
+}
 sub loadAirportInfo
 {
 	my @airportfile = readFile($_[0]);
